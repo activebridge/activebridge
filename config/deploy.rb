fa1163f796e81c69d2382ce3d2650140a3166780
Oleg Voloshyn <voloshyn@active-bridge.com>
2015-10-06 14:50:04 +0300

    add Welcome Page

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.4.0'
+lock '3.2.1'
 
 application = 'activebridge'
 user = 'deploy'
@@ -37,7 +37,7 @@ set :deploy_to, "/home/#{user}/#{application}"
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{ config/secrets.yml config/mongoid.yml }
+set :linked_files, %w{ config/secrets.yml config/database.yml }
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-
+  root to: 'welcome#index'
 end
