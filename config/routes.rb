2743eaf07499cef21237b448daee2b0eadbc8f9e
Oleg Voloshyn <voloshyn@active-bridge.com>
2015-10-07 14:26:19 +0300

    add About, Portfolio and Services partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   ActiveAdmin.routes(self)
-  get ':page', to: 'welcome#index', constraints: { page: /(team|about)/, format: 'html' }
+  get ':page', to: 'welcome#index', constraints: { page: /(team|about|services|portfolio)/, format: 'html' }
   resource :team, only: :show
   root to: 'welcome#index'
 end
