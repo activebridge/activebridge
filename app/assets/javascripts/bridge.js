8c82752fbd58bccd1e0337cde8ea816b8d49d7af
Dima Makodzeba <dima.m@active-bridge.com>
2015-10-08 19:36:09 +0300

    Fix delorian

--- app/assets/javascripts/bridge.js
+++ app/assets/javascripts/bridge.js
@@ -19,8 +19,8 @@ delorian = document.getElementById('delorian');
 
 window.onscroll = navigate;
 function navigate() {
-  k = window.innerHeight / 675
-  scroll = document.body.scrollTop / (document.body.clientHeight - window.innerHeight) * 3000/k
+  k = window.innerHeight / 675;
+  scroll = document.body.scrollTop / (document.body.clientHeight - window.innerHeight) * 3000
 
   if (scroll < 500/k) {
     step1.style.top = 0 - scroll*2*k + 'px';
@@ -28,6 +28,8 @@ function navigate() {
 
     step2.style.top = 0 - scroll*2*k + 'px';
     step2.style.bottom = 0 - scroll*2*k + 'px';
+
+    step3.style.bottom = 10 - scroll*k/50 + '%';
 
     hills.style.top = 0 - scroll*k + 'px';
     hills.style.bottom = 0 - scroll*k + 'px';
@@ -38,22 +40,25 @@ function navigate() {
     step2.style.top = 500 - scroll*3*k + 'px';
     step2.style.bottom = 1500 - scroll*5*k + 'px';
 
+    step3.style.bottom = 100 - scroll*k/5 + '%';
+
     hills.style.top = 0 - scroll*k + 'px';
     hills.style.bottom = 1000 - scroll*3*k + 'px';
   } else {
     step1.style.top = 500 - scroll*3*k + 'px';
     step1.style.bottom = 1500 - scroll*5*k + 'px';
 
-    step2.style.top = 4000 - scroll*6*k + 'px';
-    step2.style.bottom = 11100 - scroll*13*k + 'px';
+    step2.style.top = 13600 - scroll*13*k + 'px';
+    step2.style.bottom = 30900 - scroll*23*1.2*k + 'px';
+
+    step3.style.bottom = 100 - scroll*k/5 + '%';
 
     hills.style.top = 0 - scroll*k + 'px';
     hills.style.bottom = 1000 - scroll*3*k + 'px';
   }
 
-  step3.style.backgroundPositionY = 75 + scroll/6 + '%';
-  step4.style.backgroundPositionX = 70 - scroll/6 + '%';
-  step5.style.backgroundPositionX = 40 + scroll/6 + '%';
+  step4.style.backgroundPositionX = 70 - scroll/(6*k) + '%';
+  step5.style.backgroundPositionX = 40 + scroll/(6*k) + '%';
 
   front.style.opacity = 1 - scroll/100
 
@@ -61,7 +66,7 @@ function navigate() {
     contacts.style.opacity = scroll*k/2000;
     contacts.style.zoom = scroll*k / 500;
     contacts.style.margin =  '0% ' + 100 - scroll/(k*100) + '%';
-    contacts.style.top = -520 + scroll*k/6 + '%';
+    contacts.style.top = -350 + scroll*k/6 + '%';
     st = Math.random()
     if (st > 0.5) { st = st } else { st = -st}
     lightning.style.transform = "scale(1,"+ st +")";
@@ -71,6 +76,7 @@ function navigate() {
   }
 
   if (scroll > 1200/k) {
+    portfolio.style.visibility = 'visible';
     lightning.style.transform = "scale("+Math.random()*scroll/100 + "," + Math.random()*scroll/200 + ")";
     machine.style.opacity = scroll*k/1300
     portfolio.style.opacity = scroll*k/1000
@@ -78,6 +84,7 @@ function navigate() {
   } else {
     machine.style.opacity = 0;
     portfolio.style.opacity = 0;
+    portfolio.style.visibility = 'hidden';
   }
 
   if (scroll < 1300/k && scroll > 900/k) {
@@ -88,7 +95,7 @@ function navigate() {
 
   if (scroll > 1200/k) {
     delorian.style.left = 185/k - scroll*scroll*k/8000 + '%';
-    delorian.style.bottom = 125 - scroll*k/10 + '%';
+    delorian.style.top = -50 + scroll*k/15 + '%';
     delorian.style.zoom = scroll*k/12 + '%';
   } else {
     delorian.style.left = '0%';
