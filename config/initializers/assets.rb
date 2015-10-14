71cc7ea606975a6eee0867fe10da6adf014d7d82
Dima Makodzeba <dima.m@active-bridge.com>
2015-10-14 15:38:07 +0300

    Fix icomoon

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w( application.css application.js )
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
