9de6961fe5ea68bcb004857a04274c5af0056594
AlexShmatko <alex.shmatko@active-bridge.com>
2015-11-23 15:59:30 +0200

    Style fixes

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,7 +4,13 @@ class ImageUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
 
   version :mid do
-    process :resize_to_fill => [245, 245]
+    process resize_to_fill: [160, 160]
+  end
+
+  version :circle do
+    process resize_to_fill: [300, 300]
+    process convert: :png
+    cloudinary_transformation radius: 150;
   end
 
   def extension_white_list
