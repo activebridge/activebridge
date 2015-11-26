c2d40ac6487f0881477c39fa198269414731efe0
AlexShmatko <alex.shmatko@active-bridge.com>
2015-11-26 08:38:21 +0200

    Fixed members cropping

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,14 +3,9 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
 
-  version :mid do
-    process resize_to_fill: [160, 160]
-  end
-
   version :circle do
-    process resize_to_fill: [300, 300]
     process convert: :png
-    cloudinary_transformation radius: 150;
+    cloudinary_transformation :width => 300, :height => 300, radius: 150, :crop => :fill, :gravity => :face
   end
 
   def extension_white_list
