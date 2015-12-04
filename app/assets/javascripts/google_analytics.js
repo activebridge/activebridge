4d57bd8be40e6917b790be66a9a872f64e27f7ca
Darya Kuritsyna <daryakuritsyna@gmail.com>
2015-12-04 16:50:35 +0200

    add new google analytics code for tracking SPA

--- app/assets/javascripts/google_analytics.js
+++ app/assets/javascripts/google_analytics.js
@@ -1,8 +1,9 @@
-
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-50401698-1', 'auto');
-  ga('send', 'pageview');
+$.getScript 'www.google-analytics.com/analytics.js', () ->
+  window.ga = window.ga or ->
+    (ga.q = ga.q or []).push arguments
+  ga.l = +new Date
+  ga 'create', 'UA-50401698-1', 'auto'
+  ga 'send', 'pageview', '/abot'
+  ga 'send', 'pageview', '/services'
+  ga 'send', 'pageview', '/team'
+  ga 'send', 'pageview', '/portfolio'
