9e217fcc54631fc2fdb2a4e4b1c3f523be43b5b0
AlexShmatko <alex.shmatko@active-bridge.com>
2015-12-07 10:58:17 +0200

    Display project duration in words

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def display_duration_in_words(number)
+    if number.to_i < 12
+      "#{number} months"
+    else
+      "#{(number.to_f / 12).round(1)} years"
+    end
+  end
 end
