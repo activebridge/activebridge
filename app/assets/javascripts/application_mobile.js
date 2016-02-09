595655a6b7d913465faaee4645626337a6ec9364
Darya Kuritsyna <daryakuritsyna@gmail.com>
2016-02-09 11:46:54 +0200

    remove google_analytics

--- app/assets/javascripts/application_mobile.js
+++ app/assets/javascripts/application_mobile.js
@@ -2,7 +2,6 @@
 //= require jquery.nicescroll.min
 //= require mobile
 //= require cover_in_canvas
-//= require google_analytics
 //= require lazybox
 //= require portfolio
 //= require webfontloader
