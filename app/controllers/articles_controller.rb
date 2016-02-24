f15d582435728a72a70580f1a84c4775986e6aad
Darya Kuritsyna <daryakuritsyna@gmail.com>
2016-02-24 14:31:55 +0200

    Markdown, code syntax highlighting

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,13 @@
 class ArticlesController < ApplicationController
   layout 'blog'
-  load_and_authorize_resource find_by: :slug
 
   expose(:articles_by_type) { Article.by_type((params[:type] || 'done'), current_user).by_category(params[:category]).order(created_at: :desc).paginate(page: params[:page], per_page: 5) }
   expose(:article, attributes: :article_params, finder: :find_by_slug)
   expose(:popular_articles) { Article.done.order(viewed: :desc).first(3) }
   expose(:categories)
   expose(:markdown) { Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true) }
+
+  expose(:current_user) { User.last }
 
   def show
     article.increment_viewed
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,4 +19,25 @@ module ApplicationHelper
     end
     nil
   end
+
+  class CodeRayify < Redcarpet::Render::HTML
+    def block_code(code, language)
+      CodeRay.scan(code, language).div
+    end
+  end
+
+  def md(text)
+    coderayified = CodeRayify.new(:filter_html => true,
+                                :hard_wrap => true)
+    options = {
+    :fenced_code_blocks => true,
+    :no_intra_emphasis => true,
+    :autolink => true,
+    :strikethrough => true,
+    :lax_html_blocks => true,
+    :superscript => true
+    }
+    markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
+    markdown_to_html.render(text).html_safe
+  end
 end
