4155f8e474fddd2f76f4a8a6eaa22526a3002645
Darya Kuritsyna <daryakuritsyna@gmail.com>
2016-02-24 14:39:18 +0200

    fix2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -27,15 +27,15 @@ module ApplicationHelper
   end
 
   def md(text)
-    coderayified = CodeRayify.new(:filter_html => true,
-                                :hard_wrap => true)
+    coderayified = CodeRayify.new(filter_html: true,
+                                hard_wrap: true)
     options = {
-    :fenced_code_blocks => true,
-    :no_intra_emphasis => true,
-    :autolink => true,
-    :strikethrough => true,
-    :lax_html_blocks => true,
-    :superscript => true
+    fenced_code_blocks: true,
+    no_intra_emphasis: true,
+    autolink: true,
+    strikethrough: true,
+    lax_html_blocks: true,
+    superscript: true
     }
     markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
     markdown_to_html.render(text).html_safe
