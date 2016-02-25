92f23f9ca5f93b460fd2cc65ab59502268867333
Darya Kuritsyna <daryakuritsyna@gmail.com>
2016-02-25 15:57:39 +0200

    fix coderay plugin

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,6 +22,7 @@ module ApplicationHelper
 
   class CodeRayify < Redcarpet::Render::HTML
     def block_code(code, language)
+      language ||= :plaintext
       CodeRay.scan(code, language).div
     end
   end
