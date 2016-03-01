80957d81b452bd720f567628f43b4dc1546e388d
Oleg Voloshyn <voloshyn@active-bridge.com>
2016-03-01 10:26:23 +0200

    fix: doesn't close window (Portfolio page)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,14 +3,6 @@ module ApplicationHelper
     raw Rails.application.assets.find_asset(path.to_s + '.svg').to_s
   end
 
-  def display_duration_in_words(number)
-    if number.to_i < 12
-      "#{number} months"
-    else
-      "#{(number.to_f / 12).round(1)} years"
-    end
-  end
-
   def bootstrap_class_for flash_type
     { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }[flash_type.to_sym] || flash_type.to_s
   end
--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,12 @@ class Project < ActiveRecord::Base
   mount_uploader :image, ImageUploader
   validates :description, :logo, :technology, :duration, :team_size, :client, :industry, :link, :priority, :image, presence: true
   validates :priority, uniqueness: true
+
+  def project_length
+    display_duration_in_words(duration)
+  end
+
+  def display_duration_in_words(number)
+    number.to_i < 12 ? "#{number} months" : "#{(number.to_f / 12).round(1)} years"
+  end
 end
