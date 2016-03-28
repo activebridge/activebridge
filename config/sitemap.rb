781d3fb5930daabe476d78facce99da378a1f125
Darya Kuritsyna <daryakuritsyna@gmail.com>
2016-03-28 14:29:59 +0300

    change frequence and models for sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -3,20 +3,19 @@ require 'sitemap_generator'
 
 SitemapGenerator::Sitemap.default_host = 'https://active-bridge.com'
 SitemapGenerator::Sitemap.create do
-  add '/about', changefreq: 'daily', priority: 0.9
-  add '/services', changefreq: 'weekly'
-  add '/team', changefreq: 'weekly'
-  add '/portfolio', changefreq: 'weekly'
-  add '/blog', changefreq: 'weekly'
+  add '/about'
+  add '/services'
+  add '/team'
+  add '/portfolio'
 end
 SitemapGenerator::Sitemap.ping_search_engines
 
 SitemapGenerator::Sitemap.default_host = 'http://blog.active-bridge.com'
 SitemapGenerator::Sitemap.create do
   url root_url, last_mod: Time.now, changefreq: 'daily', priority: 0.9
-  url posts_url
-  Post.all.each do |post|
-    url post
+  url articles_url
+  Article.find_each do |article|
+    url article
   end
 end
 SitemapGenerator::Sitemap.ping_search_engines
