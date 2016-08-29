b136114eb9a62204c7effa384c19c162de364de1
galulex <alex@active-bridge.com>
2016-08-29 20:16:03 +0300

    Changes to portfolio and team

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -5,6 +5,11 @@ class AvatarUploader < CarrierWave::Uploader::Base
   process convert: :jpg
   process resize_to_fill: [200, 200, :face]
 
+  version :mask do
+    process convert: :png
+    cloudinary_transformation transformation: :mask
+  end
+
   def extension_white_list
     %w(jpg jpeg gif png)
   end
