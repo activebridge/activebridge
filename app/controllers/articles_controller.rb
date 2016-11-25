4da3c79250f694ca9b7993c229c09435112440c1
galulex <alex@active-bridge.com>
2016-11-25 07:52:55 +0200

    Blog action caching

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,10 @@
 class ArticlesController < ApplicationController
   layout 'blog'
+  caches_action :index, cache_path: proc { "#{params[:category]}#{articles.first&.cache_key}" }
+  caches_action :show, cache_path: proc { article.cache_key }
 
-  def show
-    article.increment_viewed
+  def index
+    params[:category] ||= 'All Posts'
   end
 
   private
@@ -18,7 +20,7 @@ class ArticlesController < ApplicationController
   helper_method :article
 
   def popular_articles
-    @popular_articles ||= Article.done.order(viewed: :desc).first(3)
+    @popular_articles ||= Article.done.includes(:member).order(viewed: :desc).first(3)
   end
   helper_method :popular_articles
 end
--- app/helpers/content_helper.rb
+++ app/helpers/content_helper.rb
@@ -2,8 +2,4 @@ module ContentHelper
   def clean_truncate(string)
     truncate(strip_tags(md(string)), length: 300)
   end
-
-  def render_category_name(value)
-    I18n.t('category').find { |category| category.last == value }&.first || 'All Posts'
-  end
 end
