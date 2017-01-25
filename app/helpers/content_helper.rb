a5fd62c6a786bf5be1f4e9dfefe0d2601afa4a1a
Kiril Cherkasov <sky7hunder@gmail.com>
2017-01-25 12:31:59 +0200

    small fixes

--- app/helpers/content_helper.rb
+++ app/helpers/content_helper.rb
@@ -2,11 +2,4 @@ module ContentHelper
   def clean_truncate(string)
     truncate(strip_tags(md(string)), length: 490)
   end
-
-  def render_empty_boxes(articles)
-    empty_boxes = 4 - articles.count
-    box = "<div class='post'></div>"
-
-    box.html_safe if empty_boxes.odd?
-  end
 end
