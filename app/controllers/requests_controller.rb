2c2211ec47c468504f06f6ac14e2bb57a2265985
galulex <alex@active-bridge.com>
2017-03-15 12:14:39 +0200

    Fix for mailer fix :)

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -2,8 +2,8 @@ class RequestsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    Request.confirm(@attrs).deliver_later
-    Request.notify(@attrs).deliver_later
+    Request.confirm(attrs).deliver_later
+    Request.notify(attrs).deliver_later
     respond_to do |f|
       f.html { redirect_to root_path }
       f.js { head 200 }
