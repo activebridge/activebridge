c5b4191d3a811bd69b453f592f763c409d55231a
galulex <alex@active-bridge.com>
2017-03-15 11:37:35 +0200

    Fix ActionController::Parameters error

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -2,11 +2,17 @@ class RequestsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    Request.confirm(params[:contact]).deliver_later
-    Request.notify(params[:contact]).deliver_later
+    Request.confirm(@attrs).deliver_later
+    Request.notify(@attrs).deliver_later
     respond_to do |f|
       f.html { redirect_to root_path }
       f.js { head 200 }
     end
+  end
+
+  private
+
+  def attrs
+    @attrs ||= params[:contact].as_json
   end
 end
