647f9263363d4778ea4c8fc7b0871891ed90cf91
Vova <volodimir.partytskyi@gmail.com>
2017-03-21 17:52:29 +0200

    Fix member placeholders

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,5 @@
 class Member < ActiveRecord::Base
+  MAX_PLACEHOLDERS_SIZE = 36
   mount_uploader :avatar, AvatarUploader
   mount_uploader :cartoon, ImageUploader
   has_many :articles
@@ -6,4 +7,8 @@ class Member < ActiveRecord::Base
   validates :name, :position, :avatar, :priority, :cartoon, presence: true
   validates :priority, uniqueness: true
   validates :cartoon, presence: true, on: :create
+
+  def self.default_placeholder(hexagon)
+    "https://res.cloudinary.com/active-bridge/image/upload/#{hexagon}/v1490098222/hd21tv2jr23hovqrfmd9.png"
+  end
 end
