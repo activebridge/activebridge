8e3e684f936ca9e29f0aac3240646555c823e611
Vova <volodimir.partytskyi@gmail.com>
2017-03-22 15:16:00 +0200

    Disable rollbar in development

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -9,6 +9,10 @@ Rollbar.configure do |config|
     config.enabled = false
   end
 
+  if Rails.env.development?
+    config.enabled = false
+  end
+
   # By default, Rollbar will try to call the `current_user` controller method
   # to fetch the logged-in user object, and then call that object's `id`,
   # `username`, and `email` methods to fetch those properties. To customize:
