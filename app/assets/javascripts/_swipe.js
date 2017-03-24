8b325c76c162ced50810b086f44fb914ba6af13d
Vova <volodimir.partytskyi@gmail.com>
2017-03-24 12:59:36 +0200

    [close #163] Fix links for mobile

--- app/assets/javascripts/_swipe.js
+++ app/assets/javascripts/_swipe.js
@@ -1,9 +1,11 @@
-var startX = startY = endX = endY = swipeX = swipeY = page = 0,
-  pages = ['home', 'about', 'services', 'testimonials', 'contact'],
+var pages = ['/', '/about', '/services', '/testimonials', '/contact'],
+  startX = startY = endX = endY = swipeX = swipeY = page = pages.indexOf(location.pathname),
   slider = document.getElementById('pages'),
   bg = document.getElementById('bg'),
   horizontal,
   swipeEndX = 100 * page;
+
+window.addEventListener('load', refreshSlide, false);
 
 slider.addEventListener('touchstart', function(event) {
     swipeX = 0;
@@ -26,7 +28,9 @@ slider.addEventListener('touchmove', function(event) {
   }
 }, false);
 
-slider.addEventListener('touchend', function(event) {
+slider.addEventListener('touchend', refreshSlide, false);
+
+function refreshSlide(event) {
   this.className = 'active'
   if (swipeX > 25 && swipeY < 10) {
     if (startX > event.changedTouches[0].pageX) {
@@ -42,7 +46,7 @@ slider.addEventListener('touchend', function(event) {
   horizontal = null;
   nav(page);
   slide(swipeX);
-}, false);
+}
 
 function slide(val) {
   slider.style.transform = 'translate(-' + (val).toFixed() + '%)';
