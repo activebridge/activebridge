df163ebaebebd6aed7ba39fa2e2a0d2e50be37ae
Alex <alex@net.ck.ua>
2018-03-02 16:53:36 +0200

    Refactor color helper

--- app/helpers/color_helper.rb
+++ app/helpers/color_helper.rb
@@ -1,20 +1,15 @@
 module ColorHelper
-  def hours_color(h)
-    working_hours = User.current_months_full_working_hours
+  def hsl_color
+    "hsl(#{@hours}, 100%, 50%)"
+  end
 
-    case
-      when h < 0.1 * working_hours then 'F8766D'
-      when h < 0.2 * working_hours then 'F98370'
-      when h < 0.3 * working_hours then 'FA9072'
-      when h < 0.4 * working_hours then 'FA9D75'
-      when h < 0.5 * working_hours then 'FBAA77'
-      when h < 0.6 * working_hours then 'FCB77A'
-      when h < 0.7 * working_hours then 'FCC47C'
-      when h < 0.8 * working_hours then 'FDD17F'
-      when h < 0.9 * working_hours then 'FEE182'
-      when h < 1.0 * working_hours then 'B1D580'
-      when h < 1.1 * working_hours then '63BE7B'
-      else '10852F'
-    end
+  def hex_color
+    hsl_color.paint.to_hex.gsub('#', '')
+  end
+
+  def hours_color(h)
+    @hours = h
+
+    hex_color
   end
 end
