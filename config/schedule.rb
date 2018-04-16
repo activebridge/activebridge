ca0e0ff1c3c49a367a80f6ba4927dd67fedc48e1
Alex Andreev <alexckua@active-bridge.com>
2018-04-16 12:43:58 +0300

    Update schedule tasks

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,12 @@
 every 1.day, at: '7:30 am' do
   rake 'sitemap:refresh'
 end
+
+every 1.day, at: '9:30 am' do
+  rake 'get_users_from_slack'
+end
+
+every 1.day, at: '10:30 am' do
+  rake 'accounting_bot'
+end
+
