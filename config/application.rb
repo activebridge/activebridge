22feba447f87ee098c87bc0bd9dcf1e5f4c99b4a
Bidofan <vip.bidochka@gmail.com>
2018-12-13 10:44:46 +0200

    Optimize site

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,7 @@ module Activebridge
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
     config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+    config.middleware.use Rack::Deflater
   end
 end
 
